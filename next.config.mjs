23473c126b2e490dd504c89acfba5de48e52ce34
jatinkala2004 <kalajatin94@gmail.com>
2025-09-17 14:15:28 +0530

    some google search favicon

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/favicon.png',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=60, must-revalidate' },
+        ],
+      },
+      {
+        source: '/favicon.ico',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=60, must-revalidate' },
+        ],
+      },
+      {
+        source: '/apple-touch-icon.png',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=60, must-revalidate' },
+        ],
+      },
+    ]
+  },
+};
 
 export default nextConfig;
